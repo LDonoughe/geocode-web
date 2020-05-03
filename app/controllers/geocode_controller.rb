96ed32f2448808dc114803bbb6e8fd9d8da7c728
Logan Donoughe <logandonoughe@gmail.com>
2020-05-02 20:21:56 -0700

    Some refactoring
    Inspired by Clean Code functions chapter

--- app/controllers/geocode_controller.rb
+++ app/controllers/geocode_controller.rb
@@ -7,32 +7,47 @@ class GeocodeController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
   def index
-    query = filtered_params['query']
     if query.present?
       response = Geocode.forward(query)
       if response && !response[:message]
-        response.delete('license')
-        response['status'] = 200
-        response['permanent_api_endpoint_location'] = 'v1/geocode/forward'
-        render json: response
+        render_information(response)
       else
-        # Error messages are parsed from the response object in lib/locationiq/geocode.rb
-        message = get_message(response)
-        status = response[:status] || 500
-        render json: { status: status,
-                       message: message,
-                       permanent_api_endpoint_location: 'v1/geocode/forward' },
-               status: status
+        render_server_error(response)
       end
     else
-      render json: { status: 400,
-                     message: 'no query provided',
-                     permanent_api_endpoint_location: 'v1/geocode/forward' },
-             status: 400
+      render_no_query_error
     end
   end
 
   private
+
+  def query
+    filtered_params['query']
+  end
+
+  def render_information(response)
+    response.delete('license')
+    response['status'] = 200
+    response['permanent_api_endpoint_location'] = 'v1/geocode/forward'
+    render json: response
+  end
+
+  def render_server_error(response)
+    # Error messages are parsed from the response object in lib/locationiq/geocode.rb
+    message = get_message(response)
+    status = response[:status] || 500
+    render json: { status: status,
+                  message: message,
+                  permanent_api_endpoint_location: 'v1/geocode/forward' },
+                  status: status
+  end
+
+  def render_no_query_error
+    render json: { status: 400,
+      message: 'no query provided',
+      permanent_api_endpoint_location: 'v1/geocode/forward' },
+      status: 400
+  end
 
   def get_message(response)
     if response.respond_to? :message
